f9e99bd256373502f8d1f54d5d10f9eca6711652
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-06-09 11:41:06 +0500

    Fix Hero

--- src/components/intro/hero/Hero.jsx
+++ src/components/intro/hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="relative bg-[url('./assets/home.jpg')] bg-no-repeat bg-cover bg-center h-screen w-full flex justify-center items-center">
       <div className='overlay absolute top-0 bottom-0 left-0 right-0 opacity-70 bg-black' />
-        <h1 className='text-center m-auto text-5xl z-10'>
+        <h1 className='text-center m-auto text-5xl md:text-7xl lg:text-9xl z-10'>
           <span className='text-white font-bold'>Special Dish</span>
           <span className='text-[#ffc515] font-bold'>.</span>
           <hr />
--- src/components/navbar/reservation/Reservation.jsx
+++ src/components/navbar/reservation/Reservation.jsx
@@ -3,7 +3,7 @@ import phone from '../../../assets/phone.png'
 
 const Reservation = () => {
   return (
-    <div className='xs:hidden sm:hidden md:hidden lg:flex justify-center items-center p-6 gap-1'>
+    <div className='xs:hidden sm:hidden md:hidden lg:flex items-center p-6 gap-1'>
       <span className='text-[#ffc515] font-bold'>Reservation</span>
       <img src={phone} alt='phone' className='w-4 h-4' />
       <span className='text-white font-bold'>03020163845</span>
