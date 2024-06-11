037b1a337c82905388169193902f05471b1e2cae
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-06-11 09:46:32 +0500

    Add Pages

--- src/components/navbar/burger/Burger.jsx
+++ src/components/navbar/burger/Burger.jsx
@@ -7,9 +7,9 @@ const Burger = ({ handleNavBtns }) => {
       className='flex flex-col justify-center items-end gap-1.5 p-6 w-full md:hidden'
       onClick={handleNavBtns}
     >
-      <div className='bg-black bg-white w-10 h-1.5 rounded' />
-      <div className='bg-black bg-white w-10 h-1.5 rounded' />
-      <div className='bg-black bg-white w-10 h-1.5 rounded' />
+      {[1, 2, 3].map(item => (
+        <div key={item} className={`bg-black bg-white w-10 h-1.5 rounded border-line-${item}`} />
+      ))} 
     </button>
   )
 }
